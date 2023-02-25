2e173d4379a06aa1fbc46e3da962a016757c54bc
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-25 20:18:50 +0530

    commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -88,7 +88,7 @@ const Dashboard = () => {
   };
 
   return (
-    <div>
+    <><div>
       <h1>Dashboard</h1>
       <h2>{goal || "No goal found"}</h2>
       <form onSubmit={addGoal}>
@@ -96,21 +96,22 @@ const Dashboard = () => {
           placeholder="Add a Goal"
           value={tempGoal}
           onChange={(e) => setTempGoal(e.target.value)}
-          type="text"
-        />
+          type="text" />
         <input type="submit" />
       </form>
-      <div id={"user-panel"}>
-                {Object.keys(user).length === 0 &&
-                <GoogleLogin id={"login-btn"} clientId={client_id} buttonText={"Log in"} isSignedIn={true}
-                             cookiePolicy={"single_host_origin"} onSuccess={onSuccess} theme={"dark"}
-                             onFailure={onFailure} scope={"profile email https://mail.google.com/"}/>}
-                {Object.keys(user).length > 0 &&
-                <GoogleLogout id={"logout-btn"} clientId={client_id} buttonText={`Logout ${user.name}`}
-                              onLogoutSuccess={handleLogOut} theme={"dark"}/>}
-            </div>
-            <EmailForm token={token}/>
     </div>
+      <div>
+        <div id={"user-panel"}>
+          {Object.keys(user).length === 0 &&
+            <GoogleLogin id={"login-btn"} clientId={client_id} buttonText={"Log in"} isSignedIn={true}
+              cookiePolicy={"single_host_origin"} onSuccess={onSuccess} theme={"dark"}
+              onFailure={onFailure} scope={"profile email https://mail.google.com/"} />}
+          {Object.keys(user).length > 0 &&
+            <GoogleLogout id={"logout-btn"} clientId={client_id} buttonText={`Logout ${user.name}`}
+              onLogoutSuccess={handleLogOut} theme={"dark"} />}
+        </div>
+        <EmailForm token={token} />
+      </div></>
   );
 };
 
