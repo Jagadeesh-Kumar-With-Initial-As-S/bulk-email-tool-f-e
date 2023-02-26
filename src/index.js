1cc67dd68b6b79662ae8add82bac758473be7bbe
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-26 10:40:54 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
-import Advertisement from "./components/Advertisement.js"
+import Advertisement from "./pages/Advertisement.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(<Advertisement/>,<App />,<Advertisement/>);
