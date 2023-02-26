4a9951fd6b4b8fac441389eb28cf4084653b7f75
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-26 19:39:43 +0530

    commit

--- src/pages/components/EmailForm.js
+++ src/pages/components/EmailForm.js
@@ -47,12 +47,14 @@ const submitbtn = async () => {
         <br />
         <br />
 
-            <attention>
+            <attention1>
             If there were more than one email address then  you may enter the email addresses with ',' inbetween the email addresses.
             <br/>
+            </attention1>
+            <attention2>
             The subject and message are same for all the email address that you may enter on the input field.
             <br/>
-            </attention>
+            </attention2>
 
         <form>
             <br />
