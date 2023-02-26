49a6b440e698ccb53ef9f3c79fe833792764e830
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-26 11:12:24 +0530

    commit

--- src/pages/Register.js
+++ src/pages/Register.js
@@ -54,9 +54,9 @@ const Register = () => {
       </form>
     </div>
     <div>
-        If you have registered already then <a href="https://bulk-email-tool-f-e.vercel.app/login">click here.
+        If you have registered already then <a href="https://bulk-email-tool-f-e.vercel.app/login">click here</a>.
           or copy and paste the following on browser to navigate to login <br/>
-          https://bulk-email-tool-f-e.vercel.app/login</a>
+          https://bulk-email-tool-f-e.vercel.app/login
     </div></>
   );
 };
