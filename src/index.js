0289d174f94dca3c5a0cdc41fe8363e992fa2121
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-26 10:32:42 +0530

    commit

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+import Advertisement from "./components/Advertisement.js"
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />);
+root.render(<Advertisement/>,<App />,<Advertisement/>);
---
+++ src/pages/Advertisement.js
@@ -0,0 +1,11 @@
+import './App.css';
+
+function Advertisement() {
+  return (
+    <div className="Advertisement">
+      Submitted and coded by Jagadeesh Kumar . S, you may send mail to my email address which is jagadeesh_2k17@proton.me and you may contribute some money to my Indian Unified Payment Interface (UPI) which is jagadeesh-kumar@ybl .
+    </div>
+  );
+}
+
+export default Advertisement;
