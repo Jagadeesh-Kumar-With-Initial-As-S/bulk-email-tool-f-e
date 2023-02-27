fb861afa276739ed44686e36269547bfad02bf19
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-27 18:13:06 +0530

    commit

--- src/pages/components/EmailForm.js
+++ src/pages/components/EmailForm.js
@@ -13,7 +13,12 @@ export default function EmailForm() {
 // var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
 
-const email_to = document.querySelector('#email-to').value;
+
+
+
+
+const onSubmit = data => {
+    const email_to = document.querySelector('#email-to').value;
 const subject = document.querySelector('#subject').value;
 const message = document.querySelector('#message').value;
 // const submit = document.querySelector('.submitted');
@@ -21,10 +26,6 @@ const mail_data = document.querySelector('.mail-data');
 var to = email_to;
 
 to = to.split(',');
-
-
-
-const onSubmit = data => {
     window.Email.send({
         SecureToken : "84212099-eb72-4b67-8505-94603fe2b0d6",
         To : to, // to include multiple emails you need to mention an array
