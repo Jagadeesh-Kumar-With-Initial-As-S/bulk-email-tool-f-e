5b87e14a8dbbff1e6afdf2df9dca53f5a812ec7a
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-27 11:48:50 +0530

    commit

--- src/pages/Dashboard.js
+++ src/pages/Dashboard.js
@@ -42,7 +42,7 @@ const Dashboard = () => {
     const req = await fetch("https://bulk-email-tool-b-k.vercel.app/api/dashboard", {
       method: "POST",
       headers: { "Content-Type": "application/json", "x-access-token": token },
-      body: JSON.stringify({
+      body: await JSON.stringify({
         tempGoal,
       }),
     });
