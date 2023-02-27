3a1975b41d8716719a1fe52609b5200c2220bebb
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-02-27 18:09:27 +0530

    commit

--- src/pages/components/EmailForm.js
+++ src/pages/components/EmailForm.js
@@ -1,5 +1,5 @@
 import React from "react";
-import test from './sendEmail';
+// import test from './sendEmail';
 
 export default function EmailForm() {
 
@@ -13,9 +13,32 @@ export default function EmailForm() {
 // var Email = { send: function (a) { return new Promise(function (n, e) { a.nocache = Math.floor(1e6 * Math.random() + 1), a.Action = "Send"; var t = JSON.stringify(a); Email.ajaxPost("https://smtpjs.com/v3/smtpjs.aspx?", t, function (e) { n(e) }) }) }, ajaxPost: function (e, n, t) { var a = Email.createCORSRequest("POST", e); a.setRequestHeader("Content-type", "application/x-www-form-urlencoded"), a.onload = function () { var e = a.responseText; null != t && t(e) }, a.send(n) }, ajax: function (e, n) { var t = Email.createCORSRequest("GET", e); t.onload = function () { var e = t.responseText; null != n && n(e) }, t.send() }, createCORSRequest: function (e, n) { var t = new XMLHttpRequest; return "withCredentials" in t ? t.open(e, n, !0) : "undefined" != typeof XDomainRequest ? (t = new XDomainRequest).open(e, n) : t = null, t } };
 
 
+const email_to = document.querySelector('#email-to').value;
+const subject = document.querySelector('#subject').value;
+const message = document.querySelector('#message').value;
+// const submit = document.querySelector('.submitted');
+const mail_data = document.querySelector('.mail-data');
+var to = email_to;
+
+to = to.split(',');
 
 
 
+const onSubmit = data => {
+    window.Email.send({
+        SecureToken : "84212099-eb72-4b67-8505-94603fe2b0d6",
+        To : to, // to include multiple emails you need to mention an array
+        From : 'jk2743@srmist.edu.in',
+        Subject : subject,
+        Body : message
+   }).then(message=>{
+    // alert(message);
+    mail_data.innerHTML = `Email was successfully sent to ${to}<br>` + mail_data.innerHTML;
+}).catch(err=>{
+    mail_data.innerHTML = 'Error sending an email!<br>' + mail_data.innerHTML;
+});
+
+}
 
 
 
@@ -86,7 +109,7 @@ export default function EmailForm() {
             <textarea placeholder="You may enter the message on here." id="message" rows="7" required></textarea>
             <br />
             <br />
-            <input type="submit" onClick={test.sendEmail()} className="submitted" value={"Send"}/>
+            <input type="submit" onClick={onSubmit} className="submitted" value={"Send"}/>
         </form>
         <div className="mail-data"></div>
     </div>
