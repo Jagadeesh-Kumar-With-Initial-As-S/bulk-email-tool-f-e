49cbe4179f3ca88db8379b22a6f8ec740840d553
Jagadeesh-Kumar-Initial-Is-S <jagadeesh_2k17@proton.me>
2023-03-01 13:30:36 +0530

    commit

--- src/pages/components/Chart001.js
+++ src/pages/components/Chart001.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import CanvasJSReact from '../../assets/canvasjs.react';
 var CanvasJSChart = CanvasJSReact.CanvasJSChart;
-console.clear();
+
 class Chart001 extends Component {
 	render() {
 		const options = {
